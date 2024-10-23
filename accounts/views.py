85db78f2eecdf2c00ee2744113485aba7ade61b2
Hallennen <hallennen.marinho3@gmail.com>
2024-10-23 12:20:04 -0300

    create project gestaoTI

---
+++ accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.views.generic.base import TemplateView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+
+# Create your views here.
+
+
+
+# class Login(TemplateView):
+#     template_name = 'login.html'
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('admin')
+        else:
+            login_form = AuthenticationForm()
+    else:
+        login_form = AuthenticationForm()
+        
+    return render(request, 'login.html', {'login_form': login_form})
